03eb6f3dd7c09d8e7378f8f885e8ff44c4f1f5ba
Murad Muradli <mmuradmuradlii@gmail.com>
2026-07-03 22:32:53 +0400

    fix: change the name of the project to planora

--- apps/api/src/email.ts
+++ apps/api/src/email.ts
@@ -4,11 +4,11 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendVerificationEmail(to: string, url: string) {
   await resend.emails.send({
-    from: "Worksprint <onboarding@resend.dev>",
+    from: "Planora <onboarding@resend.dev>",
     to,
     subject: "Verify your email address",
     html: `
-      <p>Welcome to Worksprint! Please verify your email address to activate your account.</p>
+      <p>Welcome to Planora! Please verify your email address to activate your account.</p>
       <p><a href="${url}">Verify email</a></p>
       <p>If you didn't create this account, you can ignore this email.</p>
     `,
@@ -17,11 +17,11 @@ export async function sendVerificationEmail(to: string, url: string) {
 
 export async function sendResetPasswordEmail(to: string, url: string) {
   await resend.emails.send({
-    from: "Worksprint <onboarding@resend.dev>",
+    from: "Planora <onboarding@resend.dev>",
     to,
     subject: "Reset your password",
     html: `
-      <p>We received a request to reset your Worksprint password.</p>
+      <p>We received a request to reset your Planora password.</p>
       <p><a href="${url}">Reset password</a></p>
       <p>If you didn't request this, you can safely ignore this email.</p>
     `,
